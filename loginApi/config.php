fe2809921325d3304e4c5b45e99fb2c2bf779950
DrekanZ <drekanz21@gmail.com>
2022-12-17 13:55:50 +0700

    hapus loginAPI

--- loginApi/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('SERVER','localhost');
-define('UID','root');
-define('PASSWORD','');
-define('DB_NAME','dealer');
-?>
--- loginApi/connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-include 'config.php';
-$con = mysqli_connect(SERVER, UID, PASSWORD, DB_NAME);
-?>
