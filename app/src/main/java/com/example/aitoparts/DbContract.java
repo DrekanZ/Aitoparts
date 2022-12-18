40c4cf33abf4f2dd943b4db19e4adeb9e8fcc28d
DrekanZ <drekanz21@gmail.com>
2022-12-18 17:33:15 +0700

    change IP of API

--- app/src/main/java/com/example/aitoparts/DbContract.java
+++ app/src/main/java/com/example/aitoparts/DbContract.java
@@ -1,10 +1,10 @@
 package com.example.aitoparts;
 
 public class DbContract {
-    public static final String SERVER_LOGIN_URL="https://aitoparts.galariks.my.id/loginAPI/checkLogin.php";
-    public static final String SERVER_REGISTER_URL="https://aitoparts.galariks.my.id/loginAPI/createData.php";
-    public static final String SERVER_LOADPARTS_URL="https://aitoparts.galariks.my.id/loginAPI/loadParts.php";
-    public static final String SERVER_LOADTIPS_URL="https://aitoparts.galariks.my.id/loginAPI/loadTips.php";
-    public static final String SERVER_LOADTIPSPAGE_URL="https://aitoparts.galariks.my.id/loginAPI/loadTipsPage.php";
-    public static final String SERVER_GETUSERCRED_URL="https://aitoparts.galariks.my.id/loginAPI/getUsername.php";
+    public static final String SERVER_LOGIN_URL="https://aitoparts.galariks.my.id/mobileAPI/checkLogin.php";
+    public static final String SERVER_REGISTER_URL="https://aitoparts.galariks.my.id/mobileAPI/createData.php";
+    public static final String SERVER_LOADPARTS_URL="https://aitoparts.galariks.my.id/mobileAPI/loadParts.php";
+    public static final String SERVER_LOADTIPS_URL="https://aitoparts.galariks.my.id/mobileAPI/loadTips.php";
+    public static final String SERVER_LOADTIPSPAGE_URL="https://aitoparts.galariks.my.id/mobileAPI/loadTipsPage.php";
+    public static final String SERVER_GETUSERCRED_URL="https://aitoparts.galariks.my.id/mobileAPI/getUsername.php";
 }
